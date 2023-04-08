b3f701fc37fd69a7c5bd4e3c96f0b2ca36085070
Himal Puri <purihimal9@gmail.com>
2023-04-08 17:40:27 +0200

    feat: addedItemModel

---
+++ src/main/java/np/com/lostfound/model/Item.java
@@ -0,0 +1,11 @@
+package np.com.lostfound.model;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private String name;
+    private String color;
+    private String model;
+    private String type;
+}
