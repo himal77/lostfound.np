99edc273a2775c64ddaa942d1904291f78d800b6
sushma <a01363028@unet.univie.ac.at>
2023-04-17 22:34:28 +0200

    feat: putItemById

--- src/main/java/np/com/lostfound/LostFoundApplication.java
+++ src/main/java/np/com/lostfound/LostFoundApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LostfoundApplication {
+public class LostFoundApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LostfoundApplication.class, args);
+        SpringApplication.run(LostFoundApplication.class, args);
     }
-
 }
