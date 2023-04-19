6b1cd0bece4cbc0bdec19e719fd431974437331b
sushma <a01363028@unet.univie.ac.at>
2023-04-19 17:49:10 +0200

    feat: addedEmbeddedDatabase

--- src/main/java/np/com/lostfound/model/Item.java
+++ src/main/java/np/com/lostfound/model/Item.java
@@ -1,9 +1,15 @@
 package np.com.lostfound.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "Items")
 @Data
 public class Item {
+    @Id
     private String id;
     private String name;
     private String description;
